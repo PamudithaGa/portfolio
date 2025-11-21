e46d6b2e537eadc227a9fe10ff31eda5fd7fc8b5
Pamuditha Gangana <pamudithagangana45@gmail.com>
2025-11-21 08:25:48 +0000

    Add UI part

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -5,7 +5,7 @@ const Hero: React.FC = () => {
   return (
     <div>
       <div className="w-full h-[80vh] flex  justify-center items-center text-black rounded-xl">
-        <div className=" h-full w-1/3 flex flex-col justify-center items-center">
+        <div className=" h-full w-1/3 flex flex-col justify-center items-center -mt-10">
           {" "}
           <h1 className="font-cursive font-extralight text-6xl mb-5">
             Pamuditha <span className="ml-18">Gangana</span>
@@ -19,7 +19,7 @@ const Hero: React.FC = () => {
         <div className="h-full w-1/3 flex flex-col justify-center items-center">
           <img src={MainImg} alt="Main" className="h-full" />
         </div>
-        <div className=" h-full w-1/3 flex flex-col justify-center ">
+        <div className=" h-full w-1/3 flex flex-col justify-center -mt-40">
           <h2 className="text-start w-[100%] text-5xl">
             Amid the Stom of
             <br />
@@ -28,6 +28,13 @@ const Hero: React.FC = () => {
           <h3 className="mt-6 text-lg">
             I Craft Digital Dreams with Code and Creativity.
           </h3>
+        </div>
+      </div>
+      <div className="w-full -mt-42">
+        <div className="flex justify-end">
+          <div className="border-4 border-[#222052] w-22 h-22 absolute mr-22 mt-22 z-10"></div>
+          <div className="border-4 border-[#F5CB5C] w-22 h-22 absolute mr-11 mt-11"></div>
+          <div className="border-4 border-[#222052] w-22 h-22 z-10"></div>
         </div>
       </div>
     </div>

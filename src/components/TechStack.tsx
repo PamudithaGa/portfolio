933b58c16806f2a88e925dae8eea4ae04d9c52c5
Pamuditha Gangana <pamudithagangana45@gmail.com>
2026-02-03 22:00:47 +0000

    feat: Add SpaceshipScene component and integrate it into TechStack

--- src/components/TechStack.tsx
+++ src/components/TechStack.tsx
@@ -1,5 +1,6 @@
-import React, { useState, useEffect, useRef } from "react";
+import React from "react";
 import MeImg from "../assets/me.png";
+import SpaceshipScene from "./SpaceshipScene";
 
 const TechStack: React.FC = () => {
   const tags = [
@@ -18,54 +19,46 @@ const TechStack: React.FC = () => {
     {
       name: "Python",
       src: "https://raw.githubusercontent.com/devicons/devicon/master/icons/python/python-original.svg",
+      description: "Untangles complexity with elegant grace.",
     },
     {
       name: "Node",
       src: "https://raw.githubusercontent.com/devicons/devicon/master/icons/nodejs/nodejs-original-wordmark.svg",
       className: "w-16 h-12 md:w-24 md:h-16",
+      description: "Fuels the swift, boundless race.",
     },
     {
       name: "React",
       src: "https://raw.githubusercontent.com/devicons/devicon/master/icons/react/react-original.svg",
+      description: "Paints interfaces alive and bright.",
     },
     {
       name: ".NET",
       src: "https://raw.githubusercontent.com/devicons/devicon/master/icons/dotnetcore/dotnetcore-original.svg",
+      description: "Forges backbones of might.",
     },
     {
       name: "MongoDB",
       src: "https://raw.githubusercontent.com/devicons/devicon/master/icons/mongodb/mongodb-original.svg",
+      description: "Stores data with flexible delight.",
     },
     {
       name: "Flutter",
       src: "https://raw.githubusercontent.com/devicons/devicon/master/icons/flutter/flutter-original.svg",
+      description: "Builds cross-platform dreams in plain sight.",
     },
     {
       name: "C#",
       src: "https://raw.githubusercontent.com/devicons/devicon/master/icons/csharp/csharp-original.svg",
+      description: "The sharp edge of logic and light.",
     },
     {
       name: "JS",
       src: "https://raw.githubusercontent.com/devicons/devicon/master/icons/javascript/javascript-original.svg",
       style: "bg-yellow-400 rounded-lg",
+      description: "Weaves seamless worlds in flight.",
     },
   ];
-
-  // Create a list of 12 items to fill 360 degrees (30 deg spacing)
-  // 8 unique items, so we repeat the first 4 to fill the gap seamlessly
-  const rotatingIcons = [...icons, ...icons.slice(0, 4)];
-
-  const [rotation, setRotation] = useState(0);
-  const requestRef = useRef<number>(0);
-
-  useEffect(() => {
-    const animate = () => {
-      setRotation((prev) => (prev - 0.2 + 360) % 360); // Speed of rotation (Bottom to Top)
-      requestRef.current = requestAnimationFrame(animate);
-    };
-    requestRef.current = requestAnimationFrame(animate);
-    return () => cancelAnimationFrame(requestRef.current!);
-  }, []);
 
   return (
     <div className="flex flex-col md:flex-row items-center justify-between px-10 py-20 min-h-screen bg-white overflow-hidden relative">
@@ -95,14 +88,13 @@ const TechStack: React.FC = () => {
 
       {/* Right Side: Image and Icons */}
       <div className="md:w-1/2 relative flex justify-center items-center mt-10 md:mt-0">
-        {/* The Bracket Shape */}
+        {/* The Bracket Shape - Kept for structure/background if needed, or we can hide it */}
         <div className="absolute left-1/2 -translate-x-1/2 top-1/2 -translate-y-1/2 h-[550px] w-[450px] hidden md:block z-0">
           <svg
             viewBox="0 0 450 550"
             className="h-full w-full fill-[#222052]"
             preserveAspectRatio="none"
           >
-            {/* Trapezoid: Left side tall, Right side shorter */}
             <path d="M0,0 L405,40 L405,510 L0,550 Z" />
           </svg>
         </div>
@@ -116,41 +108,20 @@ const TechStack: React.FC = () => {
           />
         </div>
 
-        {/* Floating Icons Container */}
-        <div className="absolute inset-0 flex justify-center items-center pointer-events-none z-20">
-          <div className="relative w-[350px] h-[350px] md:w-[600px] md:h-[600px]">
-            {rotatingIcons.map((icon, index) => {
-              // Calculate angle
-              const offsetAngle = index * 30; // 360 / 12 items = 30 deg
-              const currentAngle = (offsetAngle + rotation) % 360;
-
-              const angleInRad = (currentAngle * Math.PI) / 180;
-
-              // Position relative to center (50%, 50%)
-              const x = 50 + 50 * Math.cos(angleInRad);
-              const y = 50 + 50 * Math.sin(angleInRad);
-
-              const isVisible = currentAngle >= 250 || currentAngle <= 110;
-
-              return (
-                <div
-                  key={index}
-                  className="absolute transition-opacity duration-300"
-                  style={{
-                    left: `${x}%`,
-                    top: `${y}%`,
-                    transform: `translate(-50%, -50%)`,
-                    opacity: isVisible ? 1 : 0,
-                  }}
-                >
-                  <img
-                    src={icon.src}
-                    alt={icon.name}
-                    className={`w-12 h-12 md:w-16 md:h-16 ${icon.style || ""} ${icon.className || ""}`}
-                  />
-                </div>
-              );
-            })}
+        {/* Spaceship Scene Overlay */}
+        <div className="absolute inset-0 flex justify-center items-center z-20 pointer-events-none">
+          {/* Position the spaceship scene slightly higher or centered to interact with the person image? 
+                 The SpaceshipScene is 500px tall. It centers the spaceship at the top. 
+                 The person image is 500px tall. 
+                 This renders the spaceship ON TOP of the person.
+             */}
+          <div className="w-[500px] h-[600px] relative -mt-40">
+            {/* -mt-40 to pull it up so the spaceship is above the person's head if possible, 
+                     but let's just center it for now and see. 
+                     Actually, the prompt said 'Spaceship ... and alien says'.
+                     I'll position it such that the spaceship is floating above the person.
+                 */}
+            <SpaceshipScene icons={icons} />
           </div>
         </div>
       </div>
